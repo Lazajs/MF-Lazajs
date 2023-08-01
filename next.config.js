2a4526f65db269a7bba9569989f6a49d8cfb3823
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-08-01 02:22:06 -0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
+const {NextFederationPlugin} = require("@module-federation/nextjs-mf");
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    const {isServer} = options
+    config.plugins.push(
+     new NextFederationPlugin({
+        name: 'section_mf',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './Section': './src/components/Section',
+        }
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
