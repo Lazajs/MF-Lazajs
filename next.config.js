a4b0cf420a812e85411e9a089813dbdb074e50b1
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-08-04 18:59:53 -0300

    fix: module-federation

--- next.config.js
+++ next.config.js
@@ -1,16 +1,23 @@
-const {NextFederationPlugin} = require("@module-federation/nextjs-mf");
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf')
 
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
-    const {isServer} = options
+    const { isServer } = options
     config.plugins.push(
-     new NextFederationPlugin({
+      new NextFederationPlugin({
         name: 'section_mf',
         filename: 'static/chunks/remoteEntry.js',
-        exposes: {
-          './Section': './src/components/Section',
+        remotes: {
+          host: `host@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`
         },
+        exposes: {
+          './Section': './src/components/Section.tsx'
+        },
+        extraOptions: {
+          exposePages: true,
+          automaticAsyncBoundary: true
+        }
       })
     )
     return config
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,7 @@
 export default function Section () {
   return (
     <section style={{ border: '1px solid red', width: 'fit-content', margin: '50px auto', height: '250px', minHeight: 'fit-content', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ textAlign: 'center', margin: '20px' }}>This is a microfrontend! edited again</h1>
+      <h1 style={{ textAlign: 'center', margin: '20px' }}>This is a microfrontend! Coming soon :)</h1>
     </section>
   )
 }
